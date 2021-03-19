d0e894deab79cba025038b3e190fedd1e8ab8f22
VoxBorealis <uusitalo.y@gmail.com>
2021-03-19 15:44:37 +0200

    Add all basic features

--- routes.py
+++ routes.py
@@ -5,7 +5,8 @@ import tasks
 
 @app.route("/", methods=["GET","POST"])
 def index():
-    return render_template("index.html", tasks=tasks.get_all_tasks())
+    print(users.user_id())
+    return render_template("index.html", tasks=tasks.get_my_tasks(users.user_id()))
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -21,6 +22,10 @@ def logout():
     users.logout()
     return redirect("/")
 
+@app.route("/done")
+def done():
+    print("done!")
+
 @app.route("/new_task", methods=["GET", "POST"])
 def new_task():
     print("tultiin routes -> new task metodiin")
@@ -29,6 +34,17 @@ def new_task():
     print(task, priority)
     tasks.new_task(users.user_id(), task, priority)
     return redirect("/")
+
+@app.route("/tasks/<int:id>", methods=["GET","POST"])
+def task(id):
+    if request.method == "GET":
+        return render_template("task.html", id=id, creator_id=tasks.get_task_info(id)[1], task=tasks.get_task_info(id)[2],
+        priority=tasks.get_task_info(id)[3], time=tasks.get_task_info(id)[4], done=tasks.get_task_info(id)[5])
+
+    if request.method == "POST":
+        print("suorittaa done metodin")
+        tasks.task_done(id, users.user_id())
+        return redirect("/")
 
 @app.route("/register", methods=["GET","POST"])
 def register():
--- tasks.py
+++ tasks.py
@@ -16,4 +16,17 @@ def new_task(creator_id, task, priority):
 def get_all_tasks():
     sql = "SELECT id, task, creator_id FROM tasks ORDER BY priority"
     return db.session.execute(sql).fetchall()
+
+def get_my_tasks(user_id):
+    sql = "SELECT task, priority, made_at, id FROM tasks WHERE creator_id = :user_id AND done = FALSE ORDER BY priority"
+    return db.session.execute(sql, {"user_id":user_id}).fetchall()
     
+def get_task_info(id):
+    sql = "SELECT * FROM tasks WHERE id=:id"
+    return db.session.execute(sql, {"id":id}).fetchone()
+
+def task_done(task_id, user_id):
+    sql = "UPDATE tasks SET done=TRUE WHERE id=:id AND creator_id = :user_id"
+    db.session.execute(sql, {"id":task_id, "user_id":user_id})
+    db.session.commit()
+    print("done onnistui sql")
